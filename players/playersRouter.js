e806f0483a1d268a33b157119525911b097c70e3
cladams0203 <cladams0203@gmail.com>
2021-01-04 21:18:59 -0500

    finished player sockets, working on game start

--- players/playersRouter.js
+++ players/playersRouter.js
@@ -14,6 +14,55 @@ router.get("/:code", (req, res) => {
     .catch((err) => res.status(500).json({ message: err.message }));
 });
 
-router.post("/", (req, res) => {});
+router.post("/:code", async (req, res) => {
+  const newPlayer = req.body;
+  Table.findByGameCode(req.params.code)
+    .then((table) => {
+      Players.findByTableId(table.id)
+        .then((players) => {
+          if (players.length <= 9) {
+            if (players.length === 0) {
+              Players.insert({
+                ...newPlayer,
+                table_id: table.id,
+                playerId: 1,
+              })
+                .then((player) => {
+                  res
+                    .status(201)
+                    .json({ message: "player successfully joined" });
+                })
+                .catch((err) => console.log(err));
+            } else {
+              Players.insert({
+                ...newPlayer,
+                table_id: table.id,
+                playerId: players.length + 1,
+              })
+                .then((player) => {
+                  res
+                    .status(201)
+                    .json({ message: "player successfully joined" });
+                })
+                .catch((err) => console.log(err));
+            }
+          } else {
+            res.status(403).json({ message: "table is at maximum capacity" });
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          res
+            .status(500)
+            .json({ message: "unable to find players", error: err.message });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "could not find table", error: err.message });
+    });
+});
 
 module.exports = router;
